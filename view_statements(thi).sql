b4b3a11e18c57d648990fa8ce42f1a4d692f3b15
thi-tr <thi.t2103@gmail.com>
2025-12-02 18:53:25 -0500

    Create views for invoice information and producer works

---
+++ view_statements(thi).sql
@@ -0,0 +1,28 @@
+CREATE VIEW invoiceinfo
+AS 
+   SELECT i.invoice_id, s.session_id, p.project_id, artist_name, project_song_name, invoice_client_name, 
+   session_date, invoice_deadline, invoice_total_amount 
+   
+   FROM mps.invoices i
+   
+   JOIN mps.sessions s ON s.session_id = i.session_id
+   JOIN mps.projects p ON p.project_id = s.project_id
+   JOIN mps.artists a ON a.artist_id = p.artist_id
+   ORDER BY i.invoice_id, s.session_id;
+   
+SELECT * FROM mps.invoiceinfo;
+
+
+
+CREATE VIEW producerworks
+AS
+	SELECT pr.producer_id, producer_name, p.project_id, artist_name, p.project_song_name, 
+    album_name, project_status
+    
+    FROM mps.producers pr
+    
+    JOIN mps.projects p ON pr.producer_id = p.producer_id
+    JOIN mps.artists a ON a.artist_id = p.artist_id
+    ORDER BY pr.producer_id, p.project_id;
+    
+SELECT * FROM mps.producerworks;
